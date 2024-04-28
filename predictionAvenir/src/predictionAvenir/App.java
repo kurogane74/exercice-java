119bf08689321dc98e5a300ac0a413104677f188
kurogane74 <166849964+kurogane74@users.noreply.github.com>
2024-04-28 23:14:14 +0200

    Initial commit

---
+++ predictionAvenir/src/predictionAvenir/App.java
@@ -0,0 +1,48 @@
+package predictionAvenir;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int heures;
+		int minutes;
+		int plus3Minutes;
+		Scanner sc;
+			
+		sc = new Scanner(System.in);
+		
+		System.out.println("Veuillez saisir l'heure");
+		heures = sc.nextInt();
+		
+		System.out.println("Veuillez saisir les minutes");
+		minutes = sc.nextInt();
+		
+		
+		if (heures > 23 || heures < 0 || minutes > 59 || minutes < 0) {
+			System.out.println("Les données saisie sont invalides");
+		}
+
+				else {
+					
+				plus3Minutes = minutes + 3;
+
+				if (plus3Minutes >= 60) {
+					plus3Minutes -= 60;
+					heures +=1;
+				}
+				if (heures >= 24) {
+					heures = 0;
+				}
+				
+
+			
+				System.out.println("Dans 3 minutes, il sera " + heures + " h " + plus3Minutes);
+				}
+		
+		sc.close();
+
+	}
+
+}
