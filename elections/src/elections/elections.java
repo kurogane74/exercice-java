d0b7eecbc8254c09d2e2a9597594e3c24267a2ed
kurogane74 <166849964+kurogane74@users.noreply.github.com>
2024-04-29 18:42:43 +0200

    ajout exercice 2.6

---
+++ elections/src/elections/elections.java
@@ -0,0 +1,38 @@
+package elections;
+
+import java.util.Scanner;
+
+public class elections {
+
+	public static void main(String[] args) {
+		
+		String resultat = null;
+		Scanner sc;
+		sc = new Scanner(System.in);
+		System.out.print("pourcentage du candidat 1");
+		int candidat1 = sc.nextInt();
+		System.out.print("pourcentage du candidat 2");
+		int candidat2 = sc.nextInt();
+		System.out.print("pourcentage du candidat 3");
+		int candidat3 = sc.nextInt();
+		System.out.print("pourcentage du candidat 4");
+		int candidat4 = sc.nextInt();
+		
+		if (candidat1 > 50) {
+			resultat = "Vous étes elu!";
+		}
+		else if (candidat1 < 12.5) {
+			resultat = "Vous avez perdu!";
+		}
+		else if ((candidat1 > candidat2) && (candidat1 > candidat3) && (candidat1 > candidat4) && (candidat1 < 50)) {
+			resultat = "Vous étes au deuxieme tour avec avantage";
+		}
+		else if ((candidat1 < candidat2) || (candidat1 < candidat3) || (candidat1 < candidat4)) {
+			resultat = "Vous étes au deuxieme tour";
+		}
+		
+		System.out.print(resultat);
+
+	}
+
+}
