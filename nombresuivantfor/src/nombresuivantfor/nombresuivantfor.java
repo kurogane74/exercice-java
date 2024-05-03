c61ea179aeda61ab24472d5439e76d23f7bfa50b
kurogane74 <166849964+kurogane74@users.noreply.github.com>
2024-05-03 19:15:31 +0200

    les nombre d'après boucle for

---
+++ nombresuivantfor/src/nombresuivantfor/nombresuivantfor.java
@@ -0,0 +1,22 @@
+package nombresuivantfor;
+
+import java.util.Scanner;
+
+public class nombresuivantfor {
+
+	public static void main(String[] args) {
+		Scanner sc;
+		sc = new.Scanner(System.in);
+		int nombre;
+		System.out.println("entrez un nombre");
+		nombre = sc.nextInt();
+		System.out.println("Les nombres aprés " +nombre+ " sont :");
+		nombre++
+
+		for (int i = 0; i < 10; i++, nombre++) {
+			System.out.println(nombre);
+		}
+
+	}
+
+}
