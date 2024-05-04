3a217525b33ce55ca011c8e8bab392379b1800de
kurogane74 <166849964+kurogane74@users.noreply.github.com>
2024-05-04 19:40:17 +0200

    3.9

---
+++ courseschevaux/src/courseschevaux/courses.java
@@ -0,0 +1,34 @@
+package courseschevaux;
+
+import java.util.Scanner;
+
+public class courses {
+
+	public static void main(String[] args) {
+		Scanner sc;
+		sc = new Scanner(System.in);
+		System.out.println("Nombre de chevaux partants:");
+		int n = sc.nextInt();
+		System.out.println("Nombre de chevaux joué");
+		int p = sc.nextInt();
+		int factorieln = 1;
+		int factorielp = 1;
+		int factorielnp = 1;
+		int np = n - p;
+		
+		for (int i1 = 1; i1 <= n; i1++) {
+			factorieln *= i1;
+		}
+		for (int y = 1; y <= p; y++) {
+			factorielp *= y;
+		}
+		for (int z = 1; z <= np; z++) {
+			factorielnp *= z;
+		}
+		System.out.println(factorieln / factorielnp);
+		System.out.println(factorieln / (factorielp * factorielnp));
+
+		sc.close();
+	}
+
+}
